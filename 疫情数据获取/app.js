320ada585291bac15ee9f409262629699203ec2f
wdqyj-sx <wenyeqv@163.com>
2022-02-15 21:58:48 +0800

    性能优化

--- 疫情数据获取/app.js
+++ 疫情数据获取/app.js
@@ -1,7 +1,6 @@
 const axios = require('axios').default
 const fs = require('fs')
 axios.get("https://ncov.dxy.cn/ncovh5/view/pneumonia?from=dxy&source=&link=&share=").then(function(data){
-    
     if(data.status == 200){
         let html = data.data
         let index1 = html.indexOf('[{"provinceName":"台湾","provinceShortName":"台湾",')
