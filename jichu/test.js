f15b6c40f5f085b2fda0876b3b6403fb1ce84825
wdqyj-sx <wenyeqv@163.com>
2022-04-01 21:46:30 +0800

    node作业

---
+++ jichu/test.js
@@ -0,0 +1,3 @@
+	const n = 12
+    const D='2018/01/01'
+    let arr={},arr1={}
---
+++ 前端工程化/nd-cli/bin/cli.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+
+const inquirer = require("inquirer")
+
+inquirer
+    .prompt([
+        {
+            
+        }
+    ])
